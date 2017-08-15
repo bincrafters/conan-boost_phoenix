b70b6a7ccb12297d8f2ff274f16d9b85d60e8405
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:17 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostPhoenixConan(ConanFile):
     name = "Boost.Phoenix"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/phoenix"
+    url = "https://github.com/bincrafters/conan-boost-phoenix"
+    source_url = "https://github.com/boostorg/phoenix"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "phoenix"
@@ -27,7 +27,7 @@ class BoostPhoenixConan(ConanFile):
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")

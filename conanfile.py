826c974fcaae25cb9219868151cfff31a311d57c
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:29 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostPhoenixConan(ConanFile):
     name = "boost_phoenix"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_phoenix"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["phoenix"]
     is_header_only = True
 
